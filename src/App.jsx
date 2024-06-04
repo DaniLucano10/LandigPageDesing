f36f0e897103c325d15b3df4725375007e83fb8a
DaniLucano10 <dani.lucano@unmsm.edu.pe>
2024-06-04 14:21:48 -0500

    home

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Clients } from "./components/Clients"
+import { Header } from "./components/Header"
+import { Hero } from "./components/Hero"
+import { Works } from "./components/Works"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      <Hero/>
+      <Clients />
+      <Works />
+    </>
+  )
+}
+
+export default App
