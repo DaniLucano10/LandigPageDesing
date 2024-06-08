39ed755880c4943836d51c1f0335c2640c41a07e
DaniLucano10 <dani.lucano@unmsm.edu.pe>
2024-06-08 00:10:22 -0500

    Landing Page Terminada

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { Clients } from "./components/Clients"
 import { Header } from "./components/Header"
 import { Hero } from "./components/Hero"
+import { Reviews } from "./components/Reviews"
 import { Works } from "./components/Works"
+import { Services } from './components/Services';
+import { Footer } from "./components/Footer"
 
 
 function App() {
@@ -13,6 +16,9 @@ function App() {
       <Hero/>
       <Clients />
       <Works />
+      <Reviews />
+      <Services />
+      <Footer />
     </>
   )
 }
